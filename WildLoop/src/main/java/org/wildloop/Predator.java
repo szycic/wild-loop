da874ccecc022b4152d00ba8bfc5460ee9ef6423
Szymon Cichy <dev@szycic.com>
2025-05-22 20:04:27 +0200

    Poprawiona struktura klas

--- WildLoop/src/main/java/org/wildloop/Predator.java
+++ WildLoop/src/main/java/org/wildloop/Predator.java
@@ -1,4 +1,15 @@
 package org.wildloop;
 
 public class Predator extends Animal {
+    public Predator(Position position, int energy, int maxAge) {
+        super(position, energy, maxAge);
+    }
+    public Predator(Predator predator) {
+        super(predator);
+    }
+    public Predator() {
+        super();
+    }
+
+    public void hunt(Prey prey) {}
 }
--- WildLoop/src/main/java/org/wildloop/Prey.java
+++ WildLoop/src/main/java/org/wildloop/Prey.java
@@ -1,4 +1,15 @@
 package org.wildloop;
 
 public class Prey extends Animal {
+    public Prey(Position position, int energy, int maxAge) {
+        super(position, energy, maxAge);
+    }
+    public Prey(Prey prey) {
+        super(prey);
+    }
+    public Prey() {
+        super();
+    }
+
+    public void flee(Predator predator) {}
 }
