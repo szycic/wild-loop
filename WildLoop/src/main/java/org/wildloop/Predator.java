198e0c3a09509cb2bc9069a7d371b979284101f3
Szymon Cichy <dev@szycic.com>
2025-05-26 19:06:44 +0200

    Usunięcie zbędnych konstruktorów

--- WildLoop/src/main/java/org/wildloop/Predator.java
+++ WildLoop/src/main/java/org/wildloop/Predator.java
@@ -7,12 +7,6 @@ public class Predator extends Animal {
     public Predator(Position position, int energy, int maxAge) {
         super(position, energy, maxAge);
     }
-    public Predator(Predator predator) {
-        super(predator);
-    }
-    public Predator() {
-        super();
-    }
 
     @Override
     protected Direction getNextMoveDirection() {
