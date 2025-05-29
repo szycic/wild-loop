09662db30e2e81ebd756acd3828b3403a206bd5a
Szymon Cichy <dev@szycic.com>
2025-05-29 13:15:03 +0200

    Zamiana na lambdę

--- WildLoop/src/main/java/org/wildloop/Main.java
+++ WildLoop/src/main/java/org/wildloop/Main.java
@@ -7,8 +7,6 @@ import javax.swing.*;
  */
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            new StartApp();
-        });
+        SwingUtilities.invokeLater(StartApp::new);
     }
 }
