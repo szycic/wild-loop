f655884664652d8527e239dc70a082881e8f3948
Szymon Cichy <dev@szycic.com>
2025-06-25 07:50:08 +0200

    Fix javadoc

--- src/main/java/org/wildloop/SimulationPanel.java
+++ src/main/java/org/wildloop/SimulationPanel.java
@@ -7,16 +7,13 @@ import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
 /**
- * <p>
  * Represents the main graphical user interface for the simulation,
  * consisting of a grid-based world inhabited by predators and prey,
  * and provides controls for managing and displaying the simulation state.
- * </p>
  * <p>
  * This class extends {@link JPanel} and is designed to integrate with the Swing GUI framework.
  * Manages the simulation grid, statistics display, and provides user interaction
  * through controls such as pause and return buttons.
- * </p>
  *
  * @see StartApp
  * @see SimulationConfig
@@ -125,22 +122,17 @@ public class SimulationPanel extends JPanel {
     }
 
     /**
-     * <p>
      * Toggles the simulation pause state.
-     * </p>
      * <p>
      * If simulation is currently running, the method pauses simulation
      * by stopping timer and changing pause button text to indicate
      * that simulation can be resumed.
-     * </p>
      * <p>
      * If simulation is currently paused, the method resumes simulation
      * by starting the timer and updating button text to indicate
      * that simulation can be paused again.
-     * </p>
      * <p>
      * Pause state is tracked by {@code isPaused} field.
-     * </p>
      */
     private void togglePause() {
         isPaused = !isPaused; // negate current pause state
@@ -156,18 +148,14 @@ public class SimulationPanel extends JPanel {
     }
 
     /**
-     * <p>
      * Stops simulation by stopping timer and resetting pause state.
-     * </p>
      * <p>
      * Method checks if the simulation timer is active. If so, stops the timer
      * to prevent further simulation updates. Additionally, ensures the pause
      * state is set to false, indicating simulation is no longer paused.
-     * </p>
      * <p>
      * Method is called during simulation termination or reset process
      * to restore it to the default state.
-     * </p>
      */
     private void stopSimulation() {
         if (timer != null) {
@@ -209,16 +197,13 @@ public class SimulationPanel extends JPanel {
     }
 
     /**
-     * <p>
      * Starts simulation by initializing or resetting the necessary components
      * and launching the main simulation loop.
-     * </p>
      * <p>
      * Method checks if a simulation world is initialized. If not,
      * sets default simulation parameters using {@link #setSimulationParameters(int, int, int)}.
      * Ensures simulation is not paused by resetting a pause flag
      * and updates the control button to display "Pause".
-     * </p>
      * <p>
      * Main simulation loop is managed by {@link Timer} with
      * 500-millisecond delay. Each timer tick performs the following actions:
@@ -227,12 +212,10 @@ public class SimulationPanel extends JPanel {
      * <li>Updates GUI grid using {@link #updateGrid()}.</li>
      * <li>Updates simulation statistics using {@link #updateStats()}.</li>
      * </ul>
-     * </p>
      * <p>
      * If simulation reaches end condition, such as no remaining animals
      * in a world, simulation is stopped using {@link #stopSimulation()},
      * and user is shown notification.
-     * </p>
      */
     public void startSimulation() {
         // check if world exists
@@ -304,9 +287,7 @@ public class SimulationPanel extends JPanel {
     }
 
     /**
-     * <p>
      * Updates visual representation of the simulation grid.
-     * </p>
      * <p>
      * Method iterates through each cell in the simulation world grid and updates graphical
      * labels corresponding to each cell based on its current contents. Specifically:
@@ -315,7 +296,6 @@ public class SimulationPanel extends JPanel {
      *     <li>If cell contains instance of {@link Predator}, sets label text to "P"</li>
      *     <li>If cell contains instance of {@link Prey}, sets label text to "O"</li>
      * </ul>
-     * </p>
      */
     private void updateGrid() {
         for (int y = 0; y < world.getHeight(); y++) {
@@ -339,15 +319,12 @@ public class SimulationPanel extends JPanel {
     }
 
     /**
-     * <p>
      * Updates simulation statistics by counting the number of predators and prey
      * in a simulated world. Method gets a list of animals, distinguishes predators
      * and prey based on their classes and calculates their totals.
-     * </p>
      * <p>
      * Then updates statistics display with current turn number, number of
      * predators, prey and total number of animals.
-     * </p>
      */
     private void updateStats() {
         int predatorCount = 0; // predator counter
@@ -396,10 +373,8 @@ public class SimulationPanel extends JPanel {
     }
 
     /**
-     * <p>
      * Restores simulation to the initial state by stopping all active processes,
      * clearing existing simulation data and reinitializing view and necessary components.
-     * </p>
      * <p>
      * Functionality:
      * <ul>
@@ -410,20 +385,17 @@ public class SimulationPanel extends JPanel {
      * <li>Updates simulation statistics to reflect the reset state</li>
      * <li>Restores pause button label to default state</li>
      * </ul>
-     * </p>
      * <p>
      * Preconditions:
      * <ul>
      * <li>'world' object, if not {@code null}, must properly manage its animal list and turn counter</li>
      * <li>Grid graphics and statistics components must allow reinitialization</li>
      * </ul>
-     * </p>
      * <p>
      * Postconditions:
      * <ul>
      * <li>Simulation is set to clean state, ready to start a new simulation run</li>
      * </ul>
-     * </p>
      */
     public void resetSimulation() {
         if (timer != null && timer.isRunning()) {
